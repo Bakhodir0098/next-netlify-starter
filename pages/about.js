07c71169593424a3d942318bb9f2f22996cdf221
Bakhodir <bakhodir.008@Bakhodirs-MacBook-Pro.local>
2021-08-01 10:47:47 +0500

    first commit

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+
+export default function About() {
+  return (
+    <div className='container'>
+      <Head>
+        <title>About page</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main>
+        <Header title='About page' />
+        <p className='description'>About page</p>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
